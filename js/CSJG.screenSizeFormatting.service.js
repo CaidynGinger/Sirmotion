875587ab138e007236e8ecfb5ed8bb72b8d08c49
Caveman822 <caidyn.ginger@gmail.com>
2021-09-26 21:14:11 +0200

    added more to login page trying to make it responsive

---
+++ js/CSJG.screenSizeFormatting.service.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    screenSize = screen.width
+    console.log(screenSize)
+    // if(screenSize > 1000){
+    //     $("#login-h1").html(`
+    //     <h1 id="login-h1">Welcome <br> To <span class="h1PrimaryColor">Sirmotion</span></h1>
+    //     `);
+    // }
+});
