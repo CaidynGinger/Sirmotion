f2830c40b4b5ae0c23953b048ba39f44ff1202e2
Jessica van der Walt <91179414+JessicavanderWalt@users.noreply.github.com>
2021-10-03 16:21:22 +0200

    Bootstrap Basic
    Completed bootstrap for home page and individual movie page. Started with CSS for both. Added GetMovieTrailer function as well as getting the backdrop image.

--- js/JVDW.tmdb.js
+++ js/JVDW.tmdb.js
@@ -1,20 +1,39 @@
-$(document).ready(function () {
+const imgAPIBaseURL = "https://image.tmdb.org/t/p/original";
+
+$(document).ready(function() {
     // We need all the Movie Ids that we need to retrieve data for. 
-    GetMovieInformation(436970);
-    GetMovieInformation(436969);
+    GetMovieInformation(475557);
+    GetMovieTrailer(475557);
 });
 
 function GetMovieInformation(movieId) {
     // https://developers.themoviedb.org/3/movies/get-movie-details
-    $.ajax({
-        url: `https://api.themoviedb.org/3/movie/${movieId}?api_key=57db82bc0de6cdceb2fd80b8ab052215&language=en-US`
-    }).done(function (result) {
+    $.getJSON(`https://api.themoviedb.org/3/movie/${movieId}?api_key=57db82bc0de6cdceb2fd80b8ab052215&language=en-US`, function(result) {
         console.log(result);
         console.log(result.title)
         console.log(result.overview)
-        console.log(`https://image.tmdb.org/t/p/w185_and_h278_bestv2${result.poster_path}`)
+
+        let backDropPath = `${imgAPIBaseURL}${result.backdrop_path}`;
+
+        console.log(`${imgAPIBaseURL}${result.poster_path}`)
+        console.log(backDropPath)
 
         $("#movie_title").text(result.title);
-        $("#movie_img").attr("src", `https://image.tmdb.org/t/p/w185_and_h278_bestv2${result.poster_path}`);
+        $("#movie_img").attr("src", `${imgAPIBaseURL}${result.backdrop_path}`);
+        $("#home-main-movie").attr("src", backDropPath)
+        $("#individual-movie-img").attr("src", backDropPath)
+    });
+}
+
+function GetMovieTrailer(movieId) {
+    // https: //developers.themoviedb.org/3/movies/get-movie-videos
+    $.getJSON(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=57db82bc0de6cdceb2fd80b8ab052215&language=en-US`, function(result) {
+        console.log(result);
+        var mostRecentOfficialTrailers = result.results.filter(video => video.official && video.type == 'Trailer' && video.site == 'YouTube').sort(function(a, b) { return b.published_at - a.published_at });
+        console.log(mostRecentOfficialTrailers);
+        console.log("Youtube Link", `https://www.youtube.com/watch?v=${mostRecentOfficialTrailers[0].key}`);
+
+        // to get movie trailer and set src on a youtube iframe
+        // $("#movie-iframe").attr("src", `https://www.youtube.com/embed/${mostRecentOfficialTrailers[0].key}?controls=0`)
     });
 }
