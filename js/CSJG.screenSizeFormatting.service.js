457860d476b6a4bf0ea94e36832eafc86e9960ba
Caveman822 <caidyn.ginger@gmail.com>
2021-10-04 03:09:51 +0200

    added h1 tag to login page

--- js/CSJG.screenSizeFormatting.service.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    screenSize = screen.width
-    console.log(screenSize)
-    // if(screenSize > 1000){
-    //     $("#login-h1").html(`
-    //     <h1 id="login-h1">Welcome <br> To <span class="h1PrimaryColor">Sirmotion</span></h1>
-    //     `);
-    // }
-});
