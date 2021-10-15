67bed09b38699f426e3a955d91c9257768bba6fa
Caveman822 <caidyn.ginger@gmail.com>
2021-10-15 11:03:20 +0200

    added animation to login form

--- js/CSJG.formSelector.Service.js
+++ js/CSJG.formSelector.Service.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     let sideselected = 'left'
     $('.button-container').click(function (e) { 
         e.preventDefault(); 
-        console.log($('.button-selection').css('left'))
         if(sideselected === 'left'){
             $('.button-selection').css('left', '0px');
             $('.button-selection').css('border-radius', '0 5px 5px 0');
