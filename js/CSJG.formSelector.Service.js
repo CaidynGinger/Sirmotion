8a74e28c6fe1bc842c286fd465959d77850e7804
Caveman822 <caidyn.ginger@gmail.com>
2021-10-21 11:37:13 +0200

    finished login animation working on login authentication also added a gitignore

--- js/CSJG.formSelector.Service.js
+++ js/CSJG.formSelector.Service.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
-    let sideselected = 'left'
+    let sideselected = 'right'
     $('.button-container').click(function (e) { 
         e.preventDefault(); 
         if(sideselected === 'left'){
-            $('.button-selection').css('left', '0px');
+            $('.button-selection').css('left', '-1px');
             $('.button-selection').css('border-radius', '0 5px 5px 0');
+            $('#login').css('left', '0px');
+            $('#register').css('left', '1000px');
+            
             sideselected = 'right'
         } else{
             sideselected = 'left'
             $('.button-selection').css('left', '50%');
             $('.button-selection').css('border-radius', '5px 0 0 5px');
+            $('#login').css('left', '-1000px');
+            $('#register').css('left', '0px');            
+
         }
     });
 });
-
