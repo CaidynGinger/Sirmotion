9af039b1bce0c49128c9fa6247514a1361caa709
Caveman822 <caidyn.ginger@gmail.com>
2021-10-29 10:30:25 +0200

    finalized login auth for all pages

--- js/JVDW.homePage.js
+++ js/JVDW.homePage.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     GetMostPopularMovies(function(mostPopularMovies) {
         let featuredMovie = mostPopularMovies.filter(movie => movie.title.length <= 20 && movie.overview.length <= 200)[0];
         let backDropPath = `${TMDB_IMAGE_API_BASE_URL}${featuredMovie.backdrop_path}`;
-        $(".home-movie").css("background-image", `url(${backDropPath})`);
+        $(".home-movie").css("background-image", `linear-gradient(rgba(0, 0, 0, 0.50),rgba(0, 0, 0, 0.50)),url(${backDropPath})`);
+        $(".home-movie").css("background-position", `center`);
+        $(".home-movie").css("background-size", `cover`);
         $("#movie-title-home").text(featuredMovie.title);
         $(".movie-section-details-home").html(featuredMovie.overview);
 
